39e1c31426cdfbe572072cd3563f4e9ebb0bfd6a
michalav <michal.avidar@mail.huji.ac.il>
2023-06-18 09:44:02 +0300

    get_physical_address

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,6 +1,8 @@
 //
 // Created by Yair on 14-Jun-23.
 //
+#include <exception>
+#include <cassert>
 #include "VirtualMemory.h"
 
 /**
@@ -21,15 +23,75 @@ void translate(uint64_t virtualAddress, unsigned int* arr)
   }
 }
 
+bool is_frame_empty(int frameAddr) {
+  int value;
+  for (int i=0; i < TABLES_DEPTH; i++) {
+	  PMread(frameAddr + i, &value);
+	  if (value) {
+		  return false;
+	  }
+	}
+  return true;
+}
 
+void empty_frame(int frameAddr) {
+  int value;
+  for (int i=0; i < TABLES_DEPTH; i++) {
+	  PMwrite(frameAddr + i, 0);
+	}
+}
 
-uint64_t traverse(uint64_t virtualAddress)
-{
-  int[TABLES_DEPTH + 1] translated;
-  translate(virtualAddress, &translated);
+void find_empty_frame(int *newAddr, bool cleanFrame) {
+  for (int i = 0; i < ) {
+
+  }
+
+}
+
+int empty_table_handler(int *addr,  int prevAddr, bool cleanFrame){
+  int newAddr;
+  find_empty_frame(&newAddr, cleanFrame);
+  if (!newAddr) {
+	  find_frame_to_switch(&newAddr);
+	  switch_frames(&newAddr);
+  }
+
+  PMwrite (prevAddr, newAddr);
+  *addr = newAddr;
 }
 
 
+
+
+/**
+ * function extract from the page Table tree the physical address of the given
+ * virtual Address.
+ * @param virtualAddress
+ * @param pageAddress
+ * @return 0 if fails, 1 else
+ */
+int get_physical_address(uint64_t virtualAddress, uint64_t * physicalAddress) {
+
+  unsigned int translated[TABLES_DEPTH + 1];
+  translate(virtualAddress, translated);
+  int prevAddr;
+  int addr = 0;
+  for (int i = 0 ; i < TABLES_DEPTH; i++) {
+	  prevAddr = addr * PAGE_SIZE + translated[i];
+	  PMread(addr * PAGE_SIZE + translated[i], &addr);
+	  bool cleanFrame = i < TABLES_DEPTH - 1;
+	  if (!empty_table_handler(&addr, prevAddr, cleanFrame)) {
+		  return 0;
+		}
+	}
+
+  if (!empty_table_handler(&addr, prevAddr, false)) {
+	  return 0;
+	}
+
+  *physicalAddress = addr * PAGE_SIZE  + translated[TABLES_DEPTH];
+  return 1;
+}
 
 /**
  * Initialize the virtual memory.
@@ -53,8 +115,15 @@ void VMinitialize()
  */
 int VMread(uint64_t virtualAddress, word_t* value)
 {
-  uint64_t physicalAddress = traverse (virtualAddress);
-  PMread (physicalAddress, value);
+  try {
+	  uint64_t physicalAddress;
+	  assert(get_physical_address(virtualAddress, &physicalAddress) );
+	  PMread (physicalAddress, value);
+	} catch (std::exception &exception) {
+	  return 0;
+  }
+  return 1;
+
 }
 
 /**
@@ -66,8 +135,13 @@ int VMread(uint64_t virtualAddress, word_t* value)
  */
 int VMwrite(uint64_t virtualAddress, word_t value)
 {
-  uint64_t physicalAddress = traverse (virtualAddress);
-  PMwrite (physicalAddress, value);
+  try {
+	  uint64_t physicalAddress;
+	  assert(get_physical_address(virtualAddress, &physicalAddress) );
+	  PMwrite (physicalAddress, value);
+	} catch (std::exception &exception) {
+	  return 0;
+	}
+  return 1;
 }
-
 
